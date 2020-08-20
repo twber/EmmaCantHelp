977e3023642a22249caac33b70998c8fe7b5223e
twber <hctsai@umich.edu>
2020-08-20 23:12:00 +0800

    emmacanthelp827

--- EmmaCantHelp/ViewController.swift
+++ EmmaCantHelp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lable: UILabel!
+    @IBOutlet weak var duff: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        // Do any additional setup af
 }
+    
+    @IBAction func seg(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            duff.image = UIImage(named: "duffy")
+            lable.text = "DUFFY AND SHELLY MAY ARE COMING !"
+        } else if sender.selectedSegmentIndex == 1 {
+            duff.image = UIImage(named: "mickey")
+            lable.text = "MICKEY HELPS"
+         } else {
+            duff.image = UIImage(named: "daisy")
+            lable.text = "NG. KEEP SOCIAL DISTANCE PLEASE"
+       }
+    }
+    }
 
